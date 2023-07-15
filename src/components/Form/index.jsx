be4badd416421afc86a4396b3eadd351e0d661cd
MendesEduardo <eduardomn2018@gmail.com>
2023-07-14 22:48:21 -0300

    first commit

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,26 @@
+import Input from '../Input';
+
+export default function Form({ onSubmit }) {
+  const safeSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    onSubmit({ name, email });
+  };
+  return (
+    <form
+      onSubmit={safeSubmit}
+      className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5"
+    >
+      <Input type="text" required placeholder="Insira seu nome..." />
+      <Input type="email" required placeholder="Insira seu email..." />
+      <button
+        type="submit"
+        className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none hover:animate-pulse hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black"
+      >
+        Seguir
+      </button>
+    </form>
+  );
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import ToggleTheme from '../ToggleTheme';
+
+export default function Header({ user }) {
+  return (
+    <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+      <span className="text-gray-100">Olá, {user?.name || 'Usuário'}</span>
+      <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
+      <ToggleTheme className="h-8 text-gray-100 hidden sm:block cursor-pointer" />
+    </div>
+  );
+}
